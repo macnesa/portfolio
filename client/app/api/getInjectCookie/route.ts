fc4256c81ff4092942589bad232005f4faf32875
mmacnesa <mmacnesa@gmail.com>
2025-09-03 22:41:01 +0700

    fix: fix cookie middleware and schema 3

--- client/app/api/getInjectCookie/route.ts
+++ client/app/api/getInjectCookie/route.ts
@@ -2,24 +2,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const redirectTo = req.nextUrl.searchParams.get("redirectTo") || "/";
-  
+
   const response = await fetch(`${process.env.SERVER_URL}/auth/getInjectCookie`, {
     method: "GET",
-    credentials: "include",
   });
   const data = await response.json();
 
-  const token = data.data.token;
+  const token = data.data.token || null;
 
-  const res = NextResponse.redirect(redirectTo);
+  const url = new URL(redirectTo, req.nextUrl.origin);
+
+  const res = NextResponse.redirect(url);
+
   if (token) {
     res.cookies.set("accessToken", token, {
       httpOnly: true,
       secure: true,
       sameSite: "none",
       path: "/",
-      maxAge: 60 * 60 * 24,
+      maxAge: 60 * 60 * 24, // 1 hari
     });
   }
+
   return res;
 }
